cde812e763af5fe1018f32bf53fc08d6042f95a8
dudrjs145 <dudrjs145@naver.com>
2020-11-21 02:39:29 +0900

    Webpack 5 내장된 asset모듈을 사용하여 이미지 통합

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import _ from 'loadsh';
 import './style.css';
+import Img from './20x20img.png';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myImage = new Image();
+
+  myImage.src = Img;
+  myImage.width = 20;
+
+  console.log(myImage);
+
+  element.appendChild(myImage);
 
   return element;
 }
